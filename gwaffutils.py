7bbe5573228e8d8a8df6fccd3688297019ece888
bwac <bobwithacamera@gmail.com>
2020-07-04 23:14:51 +0000

    automated blackening via actions

--- gwaffutils.py
+++ gwaffutils.py
@@ -125,9 +125,7 @@ def xpgained(gwaff):
                     f"days since {list(gwaff['408355239108935681']['message_count'].keys())[0].split(' ')[0]}{config['bottom_message']}"
                 )
                 plt.ylabel("gain")
-                title = (
-                    f"ranks: {config['title']}\n{rankrange[0]}-{rankrange[1]}"
-                )
+                title = f"ranks: {config['title']}\n{rankrange[0]}-{rankrange[1]}"
                 rankrange[0] = rankrange[1]
                 rankrange[1] = rankrange[1] + config["rank_range"]
                 if config["minium_xp"] > 0:
