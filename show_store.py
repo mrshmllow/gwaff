a5601b3515d386f1bd133dc0fd7ec3a8f0774f9c
bwac <bobwithacamera@gmail.com>
2020-07-04 10:48:06 +1000

    file not needed

--- show_store.py
+++
@@ -1,8 +0,0 @@
-import gwaffutils
-import datetime
-
-new_users = gwaffutils.gethistory()
-time = datetime.datetime.today()
-print(f"{'~'*5}History{'~'*5}\n{new_users}")
-gwaff = gwaffutils.makegwaff(new_users, time)
-print(f"{'~'*5}gwaff{'~'*5}\n{gwaff}")
