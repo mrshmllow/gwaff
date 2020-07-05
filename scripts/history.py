91357b67a95702498eda3b41a153b46b1f0ecd2f
bwac <bobwithacamera@gmail.com>
2020-07-05 19:09:33 +1000

    added generate_gwaff to history.py

--- scripts/history.py
+++ scripts/history.py
@@ -1,5 +1,6 @@
 import requests
 from yaml import safe_load
+import json
 
 
 def get():
@@ -26,3 +27,33 @@ def get():
         page += 1
         i += 1
     return users
+
+
+def generate_gwaff(new_users, time):
+    with open("gwaff.json") as json_file:
+        gwaff = json.load(json_file)
+        json_file.close()
+
+    for user in new_users:
+        if new_users[user]["id"] not in gwaff:
+            gwaff[new_users[user]["id"]] = {
+                "name": f"{new_users[user]['username']}#{new_users[user]['discriminator']}",
+                "message_count": {str(time): new_users[user]["message_count"]},
+                "total_xp": {str(time): new_users[user]["xp"],},
+                "detailed_xp": {str(time): new_users[user]["detailed_xp"]},
+                "level": {str(time): new_users[user]["level"]},
+            }
+        else:
+            gwaff[new_users[user]["id"]][
+                "name"
+            ] = f"{new_users[user]['username']}#{new_users[user]['discriminator']}"
+            gwaff[new_users[user]["id"]]["message_count"][str(time)] = new_users[user][
+                "message_count"
+            ]
+            gwaff[new_users[user]["id"]]["total_xp"][str(time)] = new_users[user]["xp"]
+            gwaff[new_users[user]["id"]]["detailed_xp"][str(time)] = new_users[user][
+                "detailed_xp"
+            ]
+            gwaff[new_users[user]["id"]]["level"][str(time)] = new_users[user]["level"]
+
+    return gwaff
