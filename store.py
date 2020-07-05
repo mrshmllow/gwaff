747b8b3a06b78c7746ee7b16add7e315c50509bf
bwac <bobwithacamera@gmail.com>
2020-07-05 17:52:14 +1000

    removed history save

--- store.py
+++ store.py
@@ -4,9 +4,6 @@ import json
 
 new_users = history.get()
 time = datetime.datetime.today()
-with open(f"history/{time}.json", "w") as file:
-    json.dump(new_users, file, indent=4)
-    file.close()
 
 
 gwaff = gwaff.generate(new_users, time)
