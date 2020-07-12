272024b17c6206b6301c6acd1cf0bb36d1a44191
bwac <40532058+bwac2517@users.noreply.github.com>
2020-07-12 01:37:29 +0000

    automated blackening via actions

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 from labellines import labelLines
 from yaml import safe_load
 
+
 def bar(gwaff, save: bool = False):
     with open("config.yml", "r") as file:
         config = safe_load(file)
